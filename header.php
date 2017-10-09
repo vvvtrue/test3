7c7b98a6ecaeaf965d3476182c88d2dee174f0ed
user <user@localhost.localdomain>
2017-10-09 23:13:50 +0300

    test3

---
+++ header.php
@@ -0,0 +1,26 @@
+<html>
+  <head>
+    <title>Домашняя работа1</title>
+    <link href="style.css" rel="stylesheet">
+    <meta charset="UTF-8">
+  </head>
+  <body>
+   <div id="header">
+  	<div id="menu_div">
+        <div class="logo">
+    		  <a href=""><img src="200px-NASA_logo.svg.png" /></a>
+        </div>	
+		<div id="text_menu">
+			<a href="">Лента</a>
+			<a href="">Добавить запись</a>
+			<a href="">О проекте</a>
+		</div>		
+		<div id="search_div">
+  		<form action="">
+  			<input type="text" name="search" placeholder="Поиск...">
+  			<input type="submit" value="GO">
+  		</form>
+  		</div>
+  		</div>
+  	</div>
+  	<hr/>
