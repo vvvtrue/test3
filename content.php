fc516d59a51b08898b4f27be9063f9a2ec0fdc5f
user <user@localhost.localdomain>
2017-10-12 15:04:18 +0300

    12.10.2017

---
+++ content.php
@@ -0,0 +1,41 @@
+<?php
+$post = [
+
+
+
+
+]
+
+
+?>
+
+
+<div id="content" >
+	<div class="content_user">
+		<div class="content_header">
+			<img class="avatar" src="image/avatar1.jpg"/>  		
+			Семёнов Олег
+			<p>Два часа назад</p>
+		</div>
+		<div>
+			<img class="foto" src="246b.jpg"/>
+		</div>	  		
+			<div class="content_footer">
+  			<img class="like" src="image/image1.jpg" /> 22
+	  		<p>Отлично настроение #эмоция #зубы</p>
+  		</div>
+  </div>
+  <div class="content_user">
+		<div class="content_header">
+			<img class="avatar" src="image/avatar2.jpg"/>
+	  		Alexey2007
+	  		<p>вчера</p>
+  		</div>
+  		<img class="foto" src="foto1.jpg"/>
+  		<div  class="content_footer">
+	  		<img class="like" src="image/image2.jpg" /> 22
+	  		<p>Отлично настроение #эмоция #жизнь</p>
+  		</div>
+	</div>
+</div>
+
--- footer.php
+++ footer.php
@@ -1,34 +1,7 @@
 </div> 
   	<div>  		
   	</div>
-  	<div id="content" >
-  		<div class="content_user">
-	  		<div class="content_header">
-	  			<img class="avatar" src="http://ehoop.ru/uploads/posts/2016-02/1454720698_gomer.jpg"/>  		
-	  			Семёнов Олег
-	  			<p>Два часа назад</p>
-	  		</div>
-	  		<div>
-	  			<img class="foto" src="246b.jpg"/>
-	  		</div>	  		
-	  			<div class="content_footer">
-		  			<img class="like" src="http://www.jpeghost.ru/i/1273581745_7107_izf/10_w7j.png?1292593507" /> 22
-			  		<p>Отлично настроение #эмоция #зубы</p>
-		  		</div>
-		  </div>
-		  <div class="content_user">
-				<div class="content_header">
-					<img class="avatar" src="http://cdn01.ru/files/users/images/32/c4/32c4cb047498da9301d64986ee0a646b.jpeg"/>
-			  		Alexey2007
-			  		<p>вчера</p>
-		  		</div>
-		  		<img class="foto" src="foto1.jpg"/>
-		  		<div  class="content_footer">
-			  		<img class="like" src="http://www.jpeghost.ru/i/1273581745_7107_izf/10_w7j.png?1292593507" /> 22
-			  		<p>Отлично настроение #эмоция #жизнь</p>
-		  		</div>
-			</div>
-  	</div>
+  	
   	<div></div>
   </body>
 </html>
